6b2a62fd6cbab12b6ae0033565170362b1ac731c
ahunter <ahunter@e7d7d164-1e0e-0410-952c-ec3a4d737b14>
2005-10-25 20:39:20 +0000

    This should complete the implementation of the binder, as well as semantics for let, let* and letrec binding. Hopefully, we can now create define-syntax.

--- trunk/TameScheme/Scheme/Syntax/IBinding.cs
+++ trunk/TameScheme/Scheme/Syntax/IBinding.cs
@@ -1,3 +1,28 @@
+// +----------------------------------------------------------------------------+
+// |                               = TAMESCHEME =                               |
+// | Interface for syntax that introduces bindings                  IBinding.cs |
+// +----------------------------------------------------------------------------+
+// | Copyright (c) 2005 Andrew Hunter                                           |
+// |                                                                            |
+// | Permission is hereby granted, free of charge, to any person obtaining a    |
+// | copy of this software and associated documentation files (the "Software"), |
+// | to deal in the Software without restriction, including without limitation  |
+// | the rights to use, copy, modify, merge, publish, distribute, sublicense,   |
+// | and/or sell copies of the Software, and to permit persons to whom the      |
+// | Software is furnished to do so, subject to the following conditions:       |
+// |                                                                            |
+// | The above copyright notice and this permission notice shall be included in |
+// | all copies or substantial portions of the Software.                        |
+// |                                                                            |
+// | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR |
+// | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   |
+// | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    |
+// | THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER |
+// | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    |
+// | FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        |
+// | DEALINGS IN THE SOFTWARE.                                                  |
+// +----------------------------------------------------------------------------+
+
 using System;
 
 namespace Tame.Scheme.Syntax
@@ -12,11 +37,16 @@ namespace Tame.Scheme.Syntax
 	public interface IBinding : ISyntax
 	{
 		/// <summary>
-		/// Given scheme supposed to represent this syntax (no pattern matching will have been performed), update state to reflect the
-		/// values that have been bound.
+		/// Given some scheme and a BindingState for this particular binding environment, performs hygenic binding (converts any variable usage
+		/// to temporary variables as appropriate).
 		/// </summary>
+		/// <param name="scheme">The scheme representing the arguments for this syntax</param>
 		/// <param name="syntaxEnv">The results of matching against the patterns provided for this syntax</param>
 		/// <param name="state">The binding state for this section of scheme</param>
-		void UpdateBindingForScheme(SyntaxEnvironment syntaxEnv, Transformer.Binder.BindingState state);
+		/// <remarks>
+		/// The BindingState can be amended as required for performing suitable binding: it can also invoke the normal Binder behaviour
+		/// (which is what you normally want to do).
+		/// </remarks>
+		object BindScheme(object scheme, SyntaxEnvironment syntaxEnv, Transformer.Binder.BindingState state);
 	}
 }
