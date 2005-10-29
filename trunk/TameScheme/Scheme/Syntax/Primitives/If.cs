cec5d770ba81e997194f481c17551c057ee1ab15
ahunter <ahunter@e7d7d164-1e0e-0410-952c-ec3a4d737b14>
2005-10-29 17:00:16 +0000

    Removed some unused variables, added an initial MonoDevelop project (this is liable to get outdated for the moment, though)

--- trunk/TameScheme/Scheme/Syntax/Primitives/If.cs
+++ trunk/TameScheme/Scheme/Syntax/Primitives/If.cs
@@ -45,8 +45,6 @@ namespace Tame.Scheme.Syntax.Primitives
 
 		#region ISyntax Members
 
-		static long labelNumber = 0;
-
 		public Runtime.BExpression MakeExpression(SyntaxEnvironment env, CompileState state, int syntaxMatch)
 		{
 			object condObj = null;
@@ -71,33 +69,10 @@ namespace Tame.Scheme.Syntax.Primitives
 			BExpression thenExpr = BExpression.BuildExpression(thenObj, state);
 			BExpression elseExpr = elseObj==Data.Unspecified.Value?null:BExpression.BuildExpression(elseObj, state);
 
-#if false
-			// Labels not working??
-
-			// Labels for the various parts of the if
-			string thenLabel = "_then_" + labelNumber.ToString();
-			string finishLabel = "_finish_" + labelNumber.ToString();
-
-			labelNumber++;
-
-			// Paste them together
-			BExpression result = condExpr;													// Begin with the condition (which is not in a tail context)
-
-			result = result.Add(new Operation(Op.IfLabel, thenLabel));						// Branch to the then clause if we're successful
-			if (elseExpr != null)	
-				result = result.Add(elseExpr);												// ... if not, evaluate the 'else' clause
-			else
-				result = result.Add(new Operation(Op.Push, Data.Unspecified.Value));		// ... if there's no else clause, push an unspecified value instead
-			result = result.Add(new Operation(Op.BranchLabel, finishLabel));				// Skip to the end after the 'else' clause
-			result = result.Add(new Operation(Op.Label, thenLabel));						// 'then' portion starts here
-			result = result.Add(thenExpr);													// ... if cond is not #f, then the 'then' expression is reached
-			result = result.Add(new Operation(Op.Label, finishLabel));						// 'if' finishes here
-#else
 			// Paste the operations together
 			BExpression result = condExpr;
 
 			result = result.AddIf(thenExpr, elseExpr);
-#endif
 
 			return result;
 		}
