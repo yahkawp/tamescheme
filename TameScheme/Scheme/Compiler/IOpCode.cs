c14cef7010b6dac02ce2ff21de9c6463e40f8ad5
ahunter <ahunter@e7d7d164-1e0e-0410-952c-ec3a4d737b14>
2006-03-23 16:02:36 +0000

    Fixed the comments for some files which did not have the copyright header.

--- TameScheme/Scheme/Compiler/IOpCode.cs
+++ TameScheme/Scheme/Compiler/IOpCode.cs
@@ -1,3 +1,28 @@
+// +----------------------------------------------------------------------------+
+// |                               = TAMESCHEME =                               |
+// | Interface for classes that can compile a particular BOp         IOpCode.cs |
+// +----------------------------------------------------------------------------+
+// | Copyright (c) 2005 Andrew Hunter                                           |
+// |                                                                            |
+// | Permission is hereby granted, free of charge, to any person obtaining a    |
+// | copy of this software and associated documentation files (the "Software"), |
+// | to deal in the Software without restriction, including without limitation  |
+// | the rights to use, copy, modify, merge, publish, distribute, sublicense,   |
+// | and/or sell copies of the Software, and to permit persons to whom the      |
+// | Software is furnished to do so, subject to the following conditions:       |
+// |                                                                            |
+// | The above copyright notice and this permission notice shall be included in |
+// | all copies or substantial portions of the Software.                        |
+// |                                                                            |
+// | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR |
+// | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   |
+// | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    |
+// | THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER |
+// | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    |
+// | FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        |
+// | DEALINGS IN THE SOFTWARE.                                                  |
+// +----------------------------------------------------------------------------+
+
 using System;
 using System.Reflection.Emit;
 using Tame.Scheme.Compiler.Analysis;
@@ -10,7 +35,8 @@ namespace Tame.Scheme.Compiler
     /// </summary>
     /// <remarks>
     /// These classes are usually automatically loaded from the Scheme assembly when the Compiler class is initialised,
-    /// and are typically singleton objects. This may not be true for a highly customised scheme implementation.
+    /// and are typically singleton objects. This may not be true for a highly customised scheme implementation, which
+    /// may provide new compilers for opcodes if it wishes.
     /// </remarks>
     public interface IOpCode
     {
